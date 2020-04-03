8f3de25c3bee3fb355ebfa458f8bab22eddb47d2
ujjwal <ujjwalmaity98@gmail.com>
2020-04-03 17:45:27 +0530

    Implement the View

--- app/src/main/java/com/krishworks/countries/view/MainActivity.kt
+++ app/src/main/java/com/krishworks/countries/view/MainActivity.kt
@@ -2,12 +2,54 @@ package com.krishworks.countries.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
 import com.krishworks.countries.R
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.krishworks.countries.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ListViewModel
+    private val countryListAdapter = CountryListAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countryListAdapter
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.countries.observe(this, Observer { countries ->
+            countries?.let { countryListAdapter.updateCountries(it) }
+        })
+
+        viewModel.countryLoadError.observe(this, Observer { isError ->
+            isError?.let { errorMsg.visibility = if (it) View.VISIBLE else View.GONE }
+        })
+
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                if (it) {
+                    recyclerView.visibility = View.GONE
+                    errorMsg.visibility = View.GONE
+                    progressBar.visibility = View.VISIBLE
+                } else {
+                    recyclerView.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
+                }
+            }
+        })
     }
 }
